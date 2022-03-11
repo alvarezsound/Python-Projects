705fcc4fd360b54327a1f37b71426532bb37bc0b
alvarezsound <jeffalvarezsound@gmail.com>
2022-03-10 16:23:03 -0800

    Added inheritance assignment

---
+++ Inheritance_Assigment/Inheritance_Assignment.py
@@ -0,0 +1,16 @@
+# define parent class
+class Animal:
+    name = ' '
+    weight = 0
+    age = 0
+
+# define child class 
+class Cat(Animal):
+    catType = ' '
+    pawSize = 0
+    
+# define child class
+class Bird(Animal):
+    birdType = ' '
+    wingSpan = 0
+    
